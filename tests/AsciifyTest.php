477d192cc0abe701a2a60aebefcc330dfba2ec18
Petr Katerinak <petrkaterinak@wcaholding.cz>
2021-08-30 20:29:07 +0200

    Added ligatures

--- tests/AsciifyTest.php
+++ tests/AsciifyTest.php
@@ -19,5 +19,26 @@ class AsciifyTest extends TestCase
         // Poland
         $this->assertSame('Stroz pchnal kosc w quiz gedzb vel fax myjn.',
             Asciify::convert('Stróż pchnął kość w quiz gędźb vel fax myjń.'));
+        // Eslovenia
+        $this->assertSame('Serif bo za vajo spet kuhal domace zgance.',
+            Asciify::convert('Šerif bo za vajo spet kuhal domače žgance.'));
+        // Esperanto
+        $this->assertSame('Lau Ludoviko Zamenhof bongustas fresa ceha mangajo kun spicoj.',
+            Asciify::convert('Laŭ Ludoviko Zamenhof bongustas freŝa ĉeĥa manĝaĵo kun spicoj.'));
+        // France
+        $this->assertSame('Le coeur decu mais l\'ame plutot naive, Louys reva de crapauter en canoe au dela des iles, pres du malstron ou brulent les novae.',
+            Asciify::convert('Le cœur déçu mais l\'âme plutôt naïve, Louÿs rêva de crapaüter en canoë au delà des îles, près du mälströn où brûlent les novæ.'));
+        // Iceland
+        $this->assertSame('Kaemi ny oxi her ykist thjofum nu baedi vil og adrepa.',
+            Asciify::convert('Kæmi ný öxi hér ykist þjófum nú bæði víl og ádrepa.'));
+        // Portugal
+        $this->assertSame('Luis arguia a Julia que «bracoes, fe, cha, oxido, por, zangao» eram palavras do portugues.',
+            Asciify::convert('Luís argüia à Júlia que «brações, fé, chá, óxido, pôr, zângão» eram palavras do português.'));
+        // Turkey
+        $this->assertSame('Pijamali hasta yagiz sofore cabucak guvendi.',
+            Asciify::convert('Pijamalı hasta yağız şoföre çabucak güvendi.'));
+
+        // $this->assertSame('expected',
+        //     Asciify::convert('original'));
     }
 }
